c8eeeefe6b2ab8b83f550fb8884e13909c3c48ac
Federico Cigada <44731152+fedecigada@users.noreply.github.com>
2026-09-09 18:01:04 +0200

    Set page title and metadata, clean up root ESLint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,22 +2,13 @@ const js = require('@eslint/js');
 const globals = require('globals');
 
 module.exports = [
-  { ignores: ['node_modules/'] },
+  { ignores: ['node_modules/', 'client/'] },
   js.configs.recommended,
   {
     files: ['src/**/*.js', 'app.js'],
     languageOptions: {
       globals: {
         ...globals.node,
-      },
-    },
-  },
-  {
-    files: ['public/js/**/*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        fetchWithAuth: 'readonly',
       },
     },
   },
